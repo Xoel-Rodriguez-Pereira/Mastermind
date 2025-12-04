f95baf9ed9b0d71967c4a258e5a0cc9fcb866f79
Xoel-Rodriguez-Pereira <xoelrodriguezpereira@gmail.com>
2025-12-04 10:30:52 +0100

    feat: Complete initial unfinished module implementation

--- src/generation_generator.py
+++ src/generation_generator.py
@@ -1,11 +1,13 @@
-
-
+import random
 
 def generation_generator(generation_size, code_lenght):
 
-    generation_individuals = {}
+    pins = ('rojo', 'verde', 'azul', 'amarillo', 'roja', 'celeste', 'blanco', 'negro')
 
+    generation = {}
     for individual in generation_size:
 
-        individual_code = list(i for i in code_lenght)
-        generation_individuals[individual] = individual_code
+        individual_code = list(pins[random.randrange(0,7)] for _ in range(code_lenght))
+        generation[individual] = individual_code
+    
+    return generation
