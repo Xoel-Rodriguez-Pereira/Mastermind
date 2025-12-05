e7af05fcd2e0d6c7eb5d9f17db1f8b8b844748d8
Xoel-Rodriguez-Pereira <xoelrodriguezpereira@gmail.com>
2025-12-05 08:42:43 +0100

    feat: Finish the module generation_generator

--- src/generation_generator.py
+++ src/generation_generator.py
@@ -1,13 +1,16 @@
 import random
+from colorama import Fore
 
 def generation_generator(generation_size, code_lenght):
 
-    pins = ('rojo', 'verde', 'azul', 'amarillo', 'roja', 'celeste', 'blanco', 'negro')
+    pins = (Fore.RED + '𒊹', Fore.GREEN + '𒊹', Fore.BLUE + '𒊹',
+            Fore.YELLOW + '𒊹', Fore.MAGENTA + '𒊹', Fore.LIGHTBLUE_EX + '𒊹', 
+            Fore.WHITE + '𒊹', Fore.BLACK + '𒊹')
 
     generation = {}
-    for individual in generation_size:
+    for individual in range(generation_size):
 
-        individual_code = list(pins[random.randrange(0,7)] for _ in range(code_lenght))
+        individual_code = list(pins[random.randint(0,7)] for _ in range(code_lenght))
         generation[individual] = individual_code
-    
+
     return generation
