4796172426aeb62e8b7118876b66f8e0d04a3533
Xoel-Rodriguez-Pereira <xoelrodriguezpereira@gmail.com>
2025-12-05 14:07:02 +0100

    refactor: Change lenght of the code from 5 to 4

--- test/test_generation_generator.py
+++ test/test_generation_generator.py
@@ -4,6 +4,6 @@ import random
 
 def test_generation_generation():
 
-    generation = generation_generator (200, 5)
+    generation = generation_generator (200, 4)
     assert len(generation) == 200
-    assert len(generation[random.randint(0,200)]) == 5
+    assert len(generation[random.randint(0,200)]) == 4
