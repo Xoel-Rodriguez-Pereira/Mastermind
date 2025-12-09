ebaf3343603b9a5d5418626eebac492db1806439
Xoel-Rodriguez-Pereira <xoelrodriguezpereira@gmail.com>
2025-12-09 09:59:37 +0100

    test: Add test for generation generator

--- test/test_generation_generator.py
+++ test/test_generation_generator.py
@@ -1,9 +1,11 @@
 from src.generation_generator import generation_generator
 import random
+from colorama import Fore
 
 
 def test_generation_generation():
 
-    generation = generation_generator (200, 4)
+    generation = generation_generator (200, 4, [Fore.RED + '𒊹', Fore.RED + '𒊹', Fore.RED + '𒊹', Fore.RED + '𒊹'])
     assert len(generation) == 200
-    assert len(generation[random.randint(0,200)]) == 4
+    assert len(generation[random.randint(0,200)]) == 2
+    assert len(generation[random.randint(0,200)][0]) == 4
