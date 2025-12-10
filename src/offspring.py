69bd64d4c1032a9d294911e60f273c8cfafbfb2c
Xoel-Rodriguez-Pereira <xoelrodriguezpereira@gmail.com>
2025-12-10 13:57:34 +0100

    chore:

--- src/offspring.py
+++ src/offspring.py
@@ -1,8 +1,9 @@
 import random
 from colorama import Fore
-from generation_generator import generation_generator
+from reproduction import reproduction
+from fitness_function import fitness_function
 
-def offspring (values, fitness):
+def offspring (values, fitness, solution):
 
     SAMPLE_SIZE = 100
     selected_individuals = random.sample(list(values.keys()), SAMPLE_SIZE)
@@ -11,5 +12,10 @@ def offspring (values, fitness):
     NUM_CHILDREN = 50
     parents = [random.choices(selected_individuals, weights, k=2) for _ in range(NUM_CHILDREN)]
 
-    return parents
+    chidren_values = reproduction(parents, values)
+    children_weights = [fitness_function(solution, chidren_values[child]) for child in range(NUM_CHILDREN)]
+    
+    generation_values = values + chidren_values
+    generation_weights = weights + children_weights
 
+    
