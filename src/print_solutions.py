8f93a15105ecab20a32cd3f8dc592d8ca7f04e85
Xoel-Rodriguez-Pereira <xoelrodriguezpereira@gmail.com>
2025-12-11 09:56:08 +0100

    feat(Print solutions): Add print_solutions

---
+++ src/print_solutions.py
@@ -0,0 +1,27 @@
+from colorama import Fore
+
+def print_solutions(solutions_history, iteration):
+    
+    selected_code_string = ' '.join(item for item in solutions_history[-1][0])
+
+    if solutions_history[-1][1] == 8:
+
+        print ('''Se ha adivinado el código
+{0} {1} {2}          
+en {3} intentos.'''.format(selected_code_string, Fore.RESET + '|',solutions_history[-1][1], iteration))
+        
+    elif iteration < 12:
+        
+        print (''' Intento: {3}
+{0} {1} {2}'''.format(selected_code_string, Fore.RESET + '|',solutions_history[-1][1], iteration))
+
+    elif iteration >= 12:
+
+        print ('No se ha podido adivinar el código')
+
+
+if __name__ == '__main__':
+
+    print_solutions([([Fore.RED + '𒊹', Fore.RED + '𒊹', Fore.RED + '𒊹', Fore.RED + '𒊹'],8)], 4)
+    print_solutions([([Fore.BLUE + '𒊹', Fore.RED + '𒊹', Fore.BLUE + '𒊹', Fore.RED + '𒊹'],4)], 7)
+    print_solutions([([Fore.RED + '𒊹', Fore.RED + '𒊹', Fore.RED + '𒊹', Fore.RED + '𒊹'],0)], 12)
