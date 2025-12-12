0521cc9f2ef4489806456a6ef5e67e73a7b7b7d5
Xoel-Rodriguez-Pereira <xoelrodriguezpereira@gmail.com>
2025-12-12 13:14:59 +0100

    fix: Fix selection algorithm

--- src/offspring.py
+++ src/offspring.py
@@ -1,22 +1,18 @@
 import random
-from colorama import Fore
 from reproduction import reproduction
 from fitness_function import fitness_function
 from select_survivors import select_survivors
 
 def offspring (values, fitness, solution):
 
-    SAMPLE_SIZE = 100
-    selected_individuals = random.sample(list(values.keys()), SAMPLE_SIZE)
-    weights = list(fitness[individual] for individual in selected_individuals) 
-
     NUM_CHILDREN = 50
-    parents = [random.choices(selected_individuals, weights, k=2) for _ in range(NUM_CHILDREN)]
+    parents = [random.choices(list(fitness.keys()), list(fitness.values()), k=2) for _ in range(NUM_CHILDREN)]
 
     chidren_values = reproduction(parents, values)
-    children_weights = [fitness_function(solution, chidren_values[child]) for child in range(NUM_CHILDREN)]
+    children_fitness = [fitness_function(solution, chidren_values[child]) for child in range(NUM_CHILDREN)]
     
-    generation_values = values + chidren_values
-    generation_weights = weights + children_weights
+    generation_values = list(values.values()) + chidren_values
+    generation_fitness = list(fitness.values()) + children_fitness
 
-    return select_survivors (generation_values, generation_weights, NUM_CHILDREN)   
+    return select_survivors (generation_values, generation_fitness, NUM_CHILDREN)   
+
