eeb563bc8dffe5908a345e74f1528e88b3061f26
Xoel-Rodriguez-Pereira <xoelrodriguezpereira@gmail.com>
2025-12-16 14:03:04 +0100

    fix: Fix error with fitness results

--- src/fitness_function.py
+++ src/fitness_function.py
@@ -1,21 +1,24 @@
 
 def fitness_function (solution, individual):
 
+    individual_copy = individual[:]
     solution_copy = solution[:]
     fitness = 0
     for position in range(len(solution)): # NOPE se utiliza para evitar que un elemento en la solucón pueda aumentar 
                                           # el fitness de un individuo por repetición
-        if individual[position] == solution_copy[position]:
+        if individual_copy[position] == solution_copy[position]:
             fitness += 2  
-            solution_copy[position] == 'NOPE'
-            
-        elif individual[position] in solution_copy:
+            solution_copy[position] = 'NOPE'
+            individual_copy[position] = None
+
+    for position in range(len(solution)):
+        if individual_copy[position] != solution_copy[position] and individual_copy[position] in solution_copy:
             fitness += 1
-            solution_copy[solution_copy.index(individual[position])] = 'NOPE'
+            solution_copy[solution_copy.index(individual_copy[position])] = 'NOPE'
+            individual_copy[position] = None
 
-        else:
-            fitness += 0 
     
+    if fitness == 0:
+        fitness += 0.5
+
     return fitness
-
-
--- test/test_fitness_function.py
+++ test/test_fitness_function.py
@@ -8,3 +8,7 @@ def test_fitness_function():
     assert fitness_function([1, 1, 1, 0], [0, 0, 0, 3]) == 1
 
     assert fitness_function([1, 1, 0, 0], [1, 3, 3, 3]) == 2
+
+    assert fitness_function([3, 1, 3, 3], [1, 1, 0, 0]) == 2
+
+    assert fitness_function([0, 0, 0, 0], [2, 2, 2, 2]) == 0.5
