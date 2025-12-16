8d64e609c59cc6cc668d031612e28f47a7802c01
Xoel-Rodriguez-Pereira <xoelrodriguezpereira@gmail.com>
2025-12-16 10:51:29 +0100

    fix: Fixed algorithm

--- src/select_survivors.py
+++ src/select_survivors.py
@@ -3,10 +3,19 @@ from constants import GENERATION_SIZE
 
 def select_survivors (generation_values, generation_fitness):
 
-    individual_positions = random.choices(range(len(generation_values)), generation_fitness, k=GENERATION_SIZE)
+    individuals_list = []
+    fitness_copy = generation_fitness[:]
+    positions = list(range(len(generation_fitness)))
+    for _ in range(GENERATION_SIZE):
 
-    values = {individual : generation_values[individual] for individual in individual_positions}
+        chosen_position = random.choices(positions, fitness_copy, k=1)
+        individuals_list += chosen_position
+        positions[chosen_position[0]].pop()
+        fitness_copy[chosen_position[0]].pop()
 
-    fitness = {individual : generation_fitness[individual] for individual in individual_positions}
+
+    values = {individual : generation_values[individual] for individual in individuals_list}
+
+    fitness = {individual : generation_fitness[individual] for individual in individuals_list}
 
     return values, fitness
