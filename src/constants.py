b4fc3cb002bff17c51644499d1810109d26133cc
Xoel-Rodriguez-Pereira.com <xoelrodriguezpereira@gmail.com>
2025-12-30 15:18:05 +0100

    refactor: Change lightblue_ex > lightcyan_ex

--- src/constants.py
+++ src/constants.py
@@ -5,7 +5,7 @@ NUM_CHILDREN = GENERATION_SIZE // 4
 CODE_LENGHT = 4
 
 PINS = (Fore.RED + '𒊹', Fore.GREEN + '𒊹', Fore.BLUE + '𒊹',
-        Fore.YELLOW + '𒊹', Fore.MAGENTA + '𒊹', Fore.LIGHTBLUE_EX + '𒊹', 
+        Fore.YELLOW + '𒊹', Fore.MAGENTA + '𒊹', Fore.LIGHTCYAN_EX + '𒊹', 
         Fore.WHITE + '𒊹', Fore.BLACK + '𒊹')
 
 MAX_GENERATIONS = 14
