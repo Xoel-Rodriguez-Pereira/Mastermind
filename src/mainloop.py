4107a2ab8dec001b0bfebfb2553cb975c0576a80
Xoel-Rodriguez-Pereira.com <xoelrodriguezpereira@gmail.com>
2025-12-30 14:44:51 +0100

    feat: Add fitness grafic

--- src/mainloop.py
+++ src/mainloop.py
@@ -5,8 +5,13 @@ from src.offspring import offspring
 from src.select_representative_individual import select_representative_individual
 from src.print_solutions import print_solutions
 from src.constants import MAX_GENERATIONS 
+from src.grafics import fitness_grafic
 
 def mainloop():
+    
+    generations_almanac = []
+    fitness_almanac = []
+
     solution = correct_code()
     generation = 1
 
@@ -14,8 +19,13 @@ def mainloop():
 
     solutions_history = []
     solutions_history += select_representative_individual(values, fitness)
+
+    ## Registro para las gráficas
+    generations_almanac += [generation]
+    fitness_almanac += [list(fitness.values())]
+
     print_solutions(solutions_history, generation)
-    
+
     while generation < MAX_GENERATIONS:
 
         if solutions_history[-1][0] == solution:
@@ -23,9 +33,15 @@ def mainloop():
 
         values, fitness = offspring(values, fitness, solution)
         generation += 1
-        solutions_history = select_representative_individual(values, fitness)
-        print_solutions(solutions_history, generation)
+        solutions_history += select_representative_individual(values, fitness)
+
+        ## Registro para las gráficas
+        generations_almanac += [generation]
+        fitness_almanac += [list(fitness.values())]
         
+        print_solutions(solutions_history, generation)
+    
+    fitness_grafic(generations_almanac, fitness_almanac)
             
 
 
