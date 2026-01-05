04baaed9cde1b46d119eb9094d0ff45ee253ef93
Xoel-Rodriguez-Pereira.com <xoelrodriguezpereira@gmail.com>
2026-01-06 00:07:12 +0100

    refactor: Merge branch refactor

--- src/user_input.py
+++ src/user_input.py
@@ -2,14 +2,6 @@ from src.constants import PINS
 
 
 def correct_code():
-<<<<<<< HEAD
-
-    color_input= input("Introduce el código de 4 colores del mastermind.\n" \
-     "--------------------------------------------------------------------\n" \
-    "COLORES DISPONIBLES: Rojo, verde, azul, amarillo, rosa, celeste, blanco, negro\n" \
-     "Escribe los colores separados por espacios: ")
-=======
->>>>>>> refactor
     
     starting_silables = ("roj", "v", "az", "am", "ros", "c", "b", "n")
     associated_colours = dict(zip(starting_silables, PINS))
@@ -18,7 +10,10 @@ def correct_code():
     is_code_valid = False
     while is_code_valid == False:
          
-        color_input = input("Introduce el código de 4 colores del mastermind: ")
+        color_input= input('''Introduce el código de 4 colores del mastermind.\n" \
+     "--------------------------------------------------------------------\n" \
+    "COLORES DISPONIBLES: Rojo, verde, azul, amarillo, rosa, celeste, blanco, negro\n" \
+     "Escribe los colores separados por espacios: ''')
         user_input = color_input.split()
 
         if len(user_input) != 4:
@@ -41,6 +36,3 @@ def correct_code():
                 is_code_valid = True
 
     return user_pin_colors
-
-
-
