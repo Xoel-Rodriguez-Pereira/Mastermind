295d61d04af2b86b26ffedb1d737ceab24575449
EdMorCa67 <luigismansion12345678910@gmail.com>
2026-01-05 22:26:44 +0100

    fix:

--- src/user_input2.py
+++ src/user_input2.py
@@ -1,30 +1,26 @@
 from constants import PINS
 
-
 def correct_code():
+    color_input = input("Introduce el código de 4 colores del mastermind: ")
 
-    color_input= input("Introduce el código de 4 colores del mastermind: ")
-    
-    starting_silables=("roj","v","az","am","ros","c","b","n",)
-    associated_colours= dict(zip(starting_silables,PINS))
+    starting_silables = ("ro", "v", "az", "am", "ros", "c", "b", "n")
+    associated_colours = dict(zip(starting_silables, PINS))
 
-    user_input=color_input.split()
-    user_pin_colors=[]
+    user_input = color_input.split()
+    user_pin_colors = []
 
     if len(user_input) != 4:
-         return print("Código invalido, se necesita 4 colores")
+        return print("Código invalido, se necesita 4 colores")
 
     for item in user_input:
-     
-          for key in starting_silables:
-               if item.lower().find(key)==0:
-                    user_pin_colors+=[associated_colours[key]]
-    
-    if len (user_pin_colors) <len(user_input):
-       
-          return print("Codigo invalido")
+        found = False
+        for key in starting_silables:
+            if item.lower().startswith(key):
+                user_pin_colors.append(associated_colours[key])
+                found = True
+                break
+        if not found:
+            return print("Codigo invalido")
 
     return user_pin_colors
-
-
-
+     
